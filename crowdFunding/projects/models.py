da76e9e4a30579d265142efb82362c1603765fac
Muhammad ALsaied <muhammadalsaied96@gmail.com>
2020-03-20 23:31:43 +0200

    add projects/models.py

--- crowdFunding/projects/models.py
+++ crowdFunding/projects/models.py
@@ -2,5 +2,51 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+#from users.models import User
 # Create your models here.
+
+
+class Project(models.Model):
+    title = models.TextField(max_length=45)
+    details = models.TextField(max_length=3000)
+    target = models.IntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    rate = models.DecimalField(max_digits=2, decimal_places=2, default=0)
+    rates_count = models.IntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+
+    category = models.ForeignKey("Category", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    tags = models.ManyToManyField("Tag")
+
+
+class Category(models.Model):
+    name = models.TextField(max_length=45)
+
+
+class ProjectPicture(models.Model):
+    img_url = models.URLField()
+    Project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    name = models.TextField(max_length=45)
+
+
+class Comment(models.Model):
+    content = models.TextField(max_length=3000)
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+
+class ProjectReport(models.Model):
+    content = models.TextField(max_length=3000)
+    project = models.ForeignKey("Project", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+
+class CommentReport(models.Model):
+    content = models.TextField(max_length=3000)
+    comment = models.ForeignKey("Comment", on_delete=models.CASCADE)
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
