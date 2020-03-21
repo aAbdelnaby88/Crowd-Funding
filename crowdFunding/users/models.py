dea43b32aeaf0bb82c4f7b40585659e0c8dafb79
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-03-21 09:08:33 +0200

    update  users model

--- crowdFunding/users/models.py
+++ crowdFunding/users/models.py
@@ -3,9 +3,12 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
+
+# def __str__(self):
+#     return self.phone
 # Create your models here.
 class Users(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     email_confirmation = models.BooleanField(default=False)
     # phone_validation = RegexValidator(regex=r'^01[5|1|2|0][0-9]{8}$',
     #                              message=" Please ,, Entered the Phone number in the format: '010|212|134|156'.")
@@ -13,4 +16,5 @@ class Users(models.Model):
     facebook =  models.URLField(null=True, blank=True)
     country = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
+
+    #
