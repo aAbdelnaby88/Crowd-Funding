55180e795389b527b7fc1a963b455c030e1c7c6e
shimaa829 <shimaa.abdelazim95@gmail.com>
2020-03-31 18:44:27 +0200

    allow user to delete his account

--- crowdFunding/users/urls.py
+++ crowdFunding/users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path , include
+from django.urls import path , include , re_path
 # from django.conf.urls import url
 from . import views
 from django.contrib.auth import views as auth_views
@@ -9,7 +9,7 @@ urlpatterns = [
     path('profile/<int:uid>', views.userProfile , name='profile'),
 
     path('edit/<int:uid>', views.editProfile , name='edit'),
-
+    path('delete/<int:uid>', views.deleteuser, name="delete"),
     path('signup',views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
     path('', views.index, name='index'),
