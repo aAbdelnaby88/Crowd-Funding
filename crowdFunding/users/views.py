209ad542de2d1c214efdd60ef5967aff2b0894c3
aya morsi <ayamorsi163@gmail.com>
2020-04-04 21:05:50 +0200

    signup

--- crowdFunding/users/views.py
+++ crowdFunding/users/views.py
@@ -13,31 +13,38 @@ from django.core.mail import EmailMessage
 
 # Create your views here.
 
-
+# def custom_login(request,**kwargs):
+#     if request.user.is_authenticated:
+#         return redirect('/')
+#     else:
+#         return login(request,**kwargs)
 
 def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            email_subject = 'Activate Your Account'
-            message = render_to_string('users/activate_account.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
-                'token': account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(email_subject, message, to=[to_email])
-            email.send()
-            return HttpResponse('We have sent you an email, please confirm your email address to complete registration')
+    if request.user.is_authenticated:
+       return redirect('/')
     else:
-        form = SignUpForm()
-    context = {'form': form}
-    return render(request, 'registration/signup.html', context)
+        if request.method == 'POST':
+            form = SignUpForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+                current_site = get_current_site(request)
+                email_subject = 'Activate Your Account'
+                message = render_to_string('users/activate_account.html', {
+                    'user': user,
+                    'domain': current_site.domain,
+                    'uid': urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                    'token': account_activation_token.make_token(user),
+                })
+                to_email = form.cleaned_data.get('email')
+                email = EmailMessage(email_subject, message, to=[to_email])
+                email.send()
+                return HttpResponse('We have sent you an email, please confirm your email address to complete registration')
+        else:
+            form = SignUpForm()
+        context = {'form': form}
+        return render(request, 'registration/signup.html', context)
 
 
 def activate_account(request, uidb64, token):
@@ -46,6 +53,8 @@ def activate_account(request, uidb64, token):
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
+    if request.user.is_authenticated:
+       return redirect('/')
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
